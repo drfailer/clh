4af39de4dfa8498569d2eeec0a4980a5c5fc295b
drfailer <drfailer.42@gmail.com>
2025-11-20 10:08:47 -0500

    move ucx helper functions into a separated file

---
+++ clh/ucx.c
@@ -0,0 +1,197 @@
+#include "ucx.h"
+#include "log.h"
+
+static void failure_handler(void *request, ucp_ep_h ep, ucs_status_t status)
+{
+    fprintf(stderr, "CLH failure handler called: %s {request = %p, ep = %p}\n",
+            ucs_status_string(status), request, ep);
+}
+
+static inline CLH_Status init_ucp_context_(CLH_Handle handle)
+{
+    ucp_config_t *ucp_config;
+    if (!check_ucx(ucp_config_read("MPI", NULL, &ucp_config))) {
+        return CLH_STATUS_ERROR;
+    }
+
+    ucp_params_t ucp_params = {
+        .field_mask = UCP_PARAM_FIELD_FEATURES | UCP_PARAM_FIELD_ESTIMATED_NUM_EPS,
+        .features = UCP_FEATURE_TAG | UCP_FEATURE_WAKEUP | UCP_FEATURE_AM,
+        .estimated_num_eps = clh_nb_nodes(handle),
+    };
+    if (!check_ucx(ucp_init(&ucp_params, ucp_config, &handle->ucp_context))) {
+        ucp_config_release(ucp_config);
+        return CLH_STATUS_ERROR;
+    }
+    ucp_config_release(ucp_config);
+    return CLH_STATUS_SUCCESS;
+}
+
+static inline CLH_Status init_ucp_worker_(CLH_Handle handle)
+{
+    ucp_worker_params_t worker_params = {
+        .field_mask = UCP_WORKER_PARAM_FIELD_THREAD_MODE,
+        .thread_mode = UCS_THREAD_MODE_SINGLE,
+    };
+    if (!check_ucx(ucp_worker_create(handle->ucp_context, &worker_params, &handle->worker))) {
+        return CLH_STATUS_ERROR;
+    }
+    return CLH_STATUS_SUCCESS;
+}
+
+static inline CLH_Status init_ucp_endpoints_(CLH_Handle handle)
+{
+    if (!check_ucx(
+            ucp_worker_get_address(handle->worker, &handle->address.data, &handle->address.len))) {
+        return CLH_STATUS_ERROR;
+    }
+    int    this_node_id = clh_node_id(handle);
+    size_t nb_nodes = clh_nb_nodes(handle);
+    char  *key = clh_pmi_make_key(this_node_id);
+    char  *value = clh_pmi_make_value((char *)handle->address.data, handle->address.len);
+    clh_pmi_put(handle->pmi, key, value, handle->address.len);
+    clh_pmi_sync(handle->pmi);
+
+    handle->endpoints = malloc(nb_nodes * sizeof(ucp_ep_h));
+    for (size_t node_id = 0; node_id < nb_nodes; ++node_id) {
+        char peer_addr[1024] = {0};
+
+        if (node_id == (size_t)this_node_id) {
+            continue;
+        }
+
+        key = clh_pmi_make_key(node_id);
+        clh_pmi_get(handle->pmi, node_id, key, peer_addr, NULL);
+
+        ucp_ep_params_t ep_params = {
+            .field_mask = UCP_EP_PARAM_FIELD_REMOTE_ADDRESS | UCP_EP_PARAM_FIELD_ERR_HANDLING_MODE
+                          | UCP_EP_PARAM_FIELD_ERR_HANDLER,
+            .address = (ucp_address_t *)peer_addr,
+            .err_mode = UCP_ERR_HANDLING_MODE_PEER,
+            .err_handler.cb = &failure_handler,
+            .err_handler.arg = NULL,
+        };
+        if (!check_ucx(ucp_ep_create(handle->worker, &ep_params, &handle->endpoints[node_id]))) {
+            return CLH_STATUS_ERROR;
+        }
+    }
+    return CLH_STATUS_SUCCESS;
+}
+
+static inline CLH_Status init_cache_(CLH_Handle handle)
+{
+    handle->buffer_cache = clh_buffer_cache_create(handle->ucp_context, 8);
+    if (handle->buffer_cache == NULL) {
+        return CLH_STATUS_ERROR;
+    }
+    return CLH_STATUS_SUCCESS;
+}
+
+CLH_Status ucx_init(CLH_Handle handle)
+{
+    CLH_Status status = CLH_STATUS_SUCCESS;
+    if ((status = init_ucp_context_(handle)) != CLH_STATUS_SUCCESS) {
+        fprintf(stderr, "error: init ucp context.\n");
+        return status;
+    }
+    if ((status = init_ucp_worker_(handle)) != CLH_STATUS_SUCCESS) {
+        fprintf(stderr, "error: init ucp worker.\n");
+        return status;
+    }
+    if ((status = init_ucp_endpoints_(handle)) != CLH_STATUS_SUCCESS) {
+        fprintf(stderr, "error: init ucp endpoints.\n");
+        return status;
+    }
+    if ((status = init_cache_(handle)) != CLH_STATUS_SUCCESS) {
+        fprintf(stderr, "error: init cache.\n");
+        return status;
+    }
+    return status;
+}
+
+CLH_Status ucx_finalize(CLH_Handle handle)
+{
+    int    this_node_id = clh_node_id(handle);
+    size_t nb_nodes = clh_nb_nodes(handle);
+
+    if (!clh_buffer_cache_destroy(handle->buffer_cache)) {
+        return CLH_STATUS_ERROR;
+    }
+    for (size_t node_id = 0; node_id < nb_nodes; ++node_id) {
+        if (node_id == (size_t)this_node_id) {
+            continue;
+        }
+        ucp_request_param_t params = {
+            .op_attr_mask = UCP_OP_ATTR_FIELD_FLAGS,
+            .flags = UCP_EP_CLOSE_FLAG_FORCE,
+        };
+        ucs_status_ptr_t status_ptr = ucp_ep_close_nbx(handle->endpoints[node_id], &params);
+        if (UCS_PTR_IS_ERR(status_ptr)) {
+            return CLH_STATUS_ERROR;
+        }
+        ucx_wait(handle, status_ptr);
+    }
+    free(handle->endpoints);
+    ucp_worker_release_address(handle->worker, handle->address.data);
+    ucp_worker_destroy(handle->worker);
+    ucp_cleanup(handle->ucp_context);
+    return CLH_STATUS_SUCCESS;
+}
+
+CLH_Status ucx_wait(CLH_Handle handle, ucs_status_ptr_t status_ptr)
+{
+    if (UCS_PTR_IS_ERR(status_ptr)) {
+        return CLH_STATUS_REQUEST_FAILURE;
+    }
+
+    if (!UCS_PTR_IS_PTR(status_ptr)) {
+        return CLH_STATUS_SUCCESS;
+    }
+
+    while (true) {
+        ucs_status_t status = ucp_request_check_status(status_ptr);
+
+        if (status == UCS_INPROGRESS) {
+            ucp_worker_progress(handle->worker);
+        } else if (status == UCS_OK) {
+            break;
+        } else {
+            return CLH_STATUS_REQUEST_FAILURE;
+        }
+    }
+    ucp_request_free(status_ptr);
+    return CLH_STATUS_SUCCESS;
+}
+
+ucs_status_ptr_t ucx_send(CLH_Handle handle, CLH_Request *request, ucp_mem_h memh)
+{
+    ucp_request_param_t params = {
+        .op_attr_mask = UCP_OP_ATTR_FIELD_MEMH | UCP_OP_ATTR_FLAG_NO_IMM_CMPL,
+        .memh = memh,
+    };
+    return ucp_tag_send_nbx(handle->endpoints[request->data.send.dest],
+                            request->data.send.buffer.mem, request->data.send.buffer.len,
+                            request->data.send.tag, &params);
+}
+
+ucs_status_ptr_t ucx_recv(CLH_Handle handle, CLH_Request *request, ucp_mem_h memh)
+{
+    ucp_request_param_t params = {
+        .op_attr_mask
+        = UCP_OP_ATTR_FIELD_DATATYPE | UCP_OP_ATTR_FIELD_MEMH | UCP_OP_ATTR_FLAG_NO_IMM_CMPL,
+        .datatype = ucp_dt_make_contig(1),
+        .memh = memh,
+    };
+    ucs_status_ptr_t status;
+
+    if (request->data.recv.msg != NULL) {
+        status
+            = ucp_tag_msg_recv_nbx(handle->worker, request->data.recv.buffer.mem,
+                                   request->data.recv.buffer.len, request->data.recv.msg, &params);
+    } else {
+        status = ucp_tag_recv_nbx(handle->worker, request->data.recv.buffer.mem,
+                                  request->data.recv.buffer.len, request->data.recv.tag,
+                                  request->data.recv.tag_mask, &params);
+    }
+    return status;
+}
