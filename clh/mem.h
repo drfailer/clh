731afa664ffcdf2af8480d1fd712f125900afe26
drfailer <drfailer.42@gmail.com>
2025-12-11 10:59:56 -0500

    removed now unused memory pool

--- clh/mem.h
+++ clh/mem.h
@@ -19,36 +19,4 @@ void                *clh_system_allocator_allocate(void *, size_t size);
 void                 clh_system_allocator_free(void *, void *data);
 extern CLH_Allocator CLH_SYSTEM_ALLOCATOR;
 
-/******************************************************************************/
-/*                                dynamic pool                                */
-/******************************************************************************/
-
-struct CLH_DynMemPoolNode {
-    struct CLH_DynMemPoolNode *prev;
-    struct CLH_DynMemPoolNode *next;
-    char                       data[];
-};
-
-typedef struct {
-    struct CLH_DynMemPoolNode *used_list;
-    struct CLH_DynMemPoolNode *free_list;
-    size_t                     data_size;
-    CLH_Allocator              backing_alloctor;
-} CLH_DynMemPool;
-
-struct CLH_DynMemPoolInitArgs {
-    size_t        data_size;
-    size_t        default_capacity;
-    CLH_Allocator backing_alloctor;
-};
-void clh_dyn_mem_pool_init_(CLH_DynMemPool *pool, struct CLH_DynMemPoolInitArgs *args);
-#define clh_dyn_mem_pool_init(pool, ...)                           \
-    clh_dyn_mem_pool_init_(pool, &(struct CLH_DynMemPoolInitArgs){ \
-                                     .backing_alloctor = CLH_SYSTEM_ALLOCATOR, __VA_ARGS__})
-
-void          clh_dyn_mem_pool_destroy(CLH_DynMemPool *pool);
-void         *clh_dyn_mem_pool_alloc(CLH_DynMemPool *pool);
-void          clh_dyn_mem_pool_release(CLH_DynMemPool *pool, void *data);
-CLH_Allocator clh_dyn_mem_pool_allocator(CLH_DynMemPool *pool);
-
 #endif
